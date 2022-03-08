650476128de18476e3e4cf7a167fd72b5615127d
Akarsh Srivastava <akrsh24@gmail.com>
2022-03-08 13:12:29 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function App() {
+  return <h1>Configure Webpack</h1>;
+}
+
+ReactDom.render(<App />, document.getElementById("root"));
